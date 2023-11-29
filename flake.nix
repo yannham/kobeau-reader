0f911859c5c228a7f1f4913fe5717dc6df64c248
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2023-11-29 23:31:05 +0100

    Add initial flake.nix file

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "Download and install KOReader on a Kobo forma device.";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
